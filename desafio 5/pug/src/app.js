9924183fd9ded9ec6b6c73c3795afb1c75a3795d
Leandro Waserman <leandrowaserman@gmail.com>
2022-02-17 02:27:39 -0300

    desafio 5

---
+++ desafio 5/pug/src/app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const server = app.listen(8080,()=>console.log("Escuchando en 80800"))
+const ProductManager = require('./managers/ProductManager')
+const ProductService = new ProductManager()
+
+
+app.set('views','./src/views')
+app.set('view engine','pug')
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+
+let products= []
+function getAllItems(){    
+    ProductService.getAll().then(result=>products=result)
+    return products
+}
+
+app.get('/',(req,res)=>{
+    res.render('home')
+})
+app.get('/products',(req,res)=>{
+    
+    res.render('products',{
+        products:getAllItems()
+    })
+})
+app.post('/products',(req,res)=>{
+    ProductService.add(req.body).then(res.redirect('/products')).then(res=>res.send(getAllItems()))
+})
